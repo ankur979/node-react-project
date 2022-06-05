e1cb3c040d6c3eef9193d53a06bd6a5cdbaee083
Ankur <cwp.ankur@gmail.com>
2022-06-05 14:02:14 +0530

    build folder

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require("cors")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, './e-comm/build')))
+app.use(express.static(path.join(__dirname, 'build')))
 app.get(("/"), (req, res) => {
-    res.sendFile(path.join(__dirname, 'app/../e-comm/build/index.html'))
+    res.sendFile(path.join(__dirname, './build/index.html'))
 })
 
 app.use("/dashboard/users", require("./Router/User"))
